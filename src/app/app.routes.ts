8a0a67d47eaf7a58c0fdf39658f7f6dc335c8372
jhonatanbc <zjhonatan7@gmail.com>
2024-04-06 23:15:26 -0500

    add component login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { PersonaComponent } from './persona/persona.component';
 import { FormPersonaComponent } from './form-persona/form-persona.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
     {path : 'persona', component : PersonaComponent},
     {path : 'form-persona', component : FormPersonaComponent},
     {path : 'edit/:id', component : FormPersonaComponent},
+    {path : '', component : LoginComponent},
 ];
